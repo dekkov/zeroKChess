41eac33750c8d2a6958436867c294f6579644904
Hoang Tran <trhoang2003@gmail.com>
2024-09-26 22:27:43 -0700

    real generating training set

--- generate_training_set.py
+++ generate_training_set.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python3
+import os
 import chess.pgn
-import os 
-from state import State 
+import numpy as np
+from state import State
 
-pgn  = open("../chessdb/DATABASE4U.pgn")
-
-while 1:
-    try:
-        game = chess.pgn.read_game(pgn)
-    except:
+def get_dataset(num_samples=None):
+  X,Y = [], []
+  gn = 0
+  values = {'1/2-1/2':0, '0-1':-1, '1-0':1}
+  # pgn files in the data folder
+  for fn in os.listdir("data"):
+    pgn = open(os.path.join("data", fn))
+    while 1:
+      game = chess.pgn.read_game(pgn)
+      if game is None:
         break
-    if game.headers['Result'] == '*':
+      res = game.headers['Result']
+      if res not in values:
         continue
-    values = {"1/2-1/2": 0, "1-0": 1, "0-1" : -1}[game.headers['Result']]
-  
-    board = game.board()
-    for i, move in enumerate(game.mainline_moves()) :
-        board.push(move) 
-        print(State(board).serialize()[:,:,0])
-    # exit(0)  
- 
+      value = values[res]
+      board = game.board()
+      for i, move in enumerate(game.mainline_moves()):
+        board.push(move)
+        ser = State(board).serialize()
+        X.append(ser)
+        Y.append(value)
+      print("parsing game %d, got %d examples" % (gn, len(X)))
+      if num_samples is not None and len(X) > num_samples:
+        return X,Y
+      gn += 1
+  X = np.array(X)
+  Y = np.array(Y)
+  return X,Y
+
+if __name__ == "__main__":
+  X,Y = get_dataset(25000000)
+  np.savez("processed/dataset_25M.npz", X, Y)
